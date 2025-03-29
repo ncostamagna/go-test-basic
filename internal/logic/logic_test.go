12997f609627b99fb294ea250c57b55690cf223a
Nahue <nlcostamagna@gmail.com>
2025-03-29 15:38:59 -0300

    adding testify package

--- internal/logic/logic_test.go
+++ internal/logic/logic_test.go
@@ -2,29 +2,24 @@ package logic
 
 import (
 	"testing"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestSum(t *testing.T) {
 	result := Sum(2, 3)
 	expected := 5
 
-	if result != expected {
-		t.Errorf("Sum(2, 3) = %d; expected %d", result, expected)
-	}
+	assert.Equal(t, expected, result)
 
 	result = internalSum(1, 2, 3, 4, 5)
 	expected = 15
 
-	if result != expected {
-		t.Errorf("Sum(1, 2, 3, 4, 5) = %d; expected %d", result, expected)
-	}
+	assert.Equal(t, expected, result)
 }
 
 func TestCheckEmail(t *testing.T) {
 	result := CheckEmail("test@test.com")
 	expected := true
 
-	if result != expected {
-		t.Errorf("CheckEmail(\"test@test.com\") = %t; expected %t", result, expected)
-	}
+	assert.Equal(t, expected, result)
 }
